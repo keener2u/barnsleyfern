19a0fadcf4ea878fa6be8000a231c9faded8825d
keener2u <keener2u@penguin>
2019-12-05 04:12:31 -0500

    built react wrapper around rosettacode.org's Javascript Barnsley Fern generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import BarnsleyFern from './BarnsleyFern';
+function pBarnsleyFern(canvasId,lim) {
+  // DCLs
+  var canvas = document.getElementById(canvasId);
+  var ctx = canvas.getContext("2d");
+  var w = canvas.width;
+  var h = canvas.height;
+  var x=0.,y=0.,xw=0.,yw=0.,r;
+  // Like in PARI/GP: return random number 0..max-1
+  function randgp(max) {return Math.floor(Math.random()*max)}
+  // Clean canvas
+  ctx.fillStyle="white"; ctx.fillRect(0,0,w,h);
+  // MAIN LOOP
+  for(var i=0; i<lim; i++) {
+    r=randgp(100);
+    if (r<=1) {xw=0;yw=0.16*y;}
+    else if (r<=8) {xw=0.2*x-0.26*y;yw=0.23*x+0.22*y+1.6;}
+    else if (r<=15) {xw=-0.15*x+0.28*y;yw=0.26*x+0.24*y+0.44;}
+    else {xw=0.85*x+0.04*y;yw=-0.04*x+0.85*y+1.6;}
+    x=xw;y=yw; ctx.fillStyle="green"; ctx.fillRect(x*50+260,-y*50+540,1,1);
+  }//fend i
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+  <BarnsleyFern/>
+  </div>
   );
 }
 
---
+++ src/BarnsleyFern.js
@@ -0,0 +1,36 @@
+import React from 'react';
+class BarnsleyFern extends React.Component {
+    componentDidMount() {
+      const canvas = this.refs.canvas
+      const ctx = canvas.getContext("2d")
+      const img = this.refs.image
+      img.onload = () => {
+        var lim = 1000000;
+        var w = canvas.width;
+        var h = canvas.height;
+        var x=0.,y=0.,xw=0.,yw=0.,r;
+        // Like in PARI/GP: return random number 0..max-1
+        function randgp(max) {return Math.floor(Math.random()*max)}
+        // Clean canvas
+        ctx.fillStyle="white"; ctx.fillRect(0,0,w,h);
+        // MAIN LOOP
+        for(var i=0; i<lim; i++) {
+          r=randgp(100);
+          if (r<=1) {xw=0;yw=0.16*y;}
+          else if (r<=8) {xw=0.2*x-0.26*y;yw=0.23*x+0.22*y+1.6;}
+          else if (r<=15) {xw=-0.15*x+0.28*y;yw=0.26*x+0.24*y+0.44;}
+          else {xw=0.85*x+0.04*y;yw=-0.04*x+0.85*y+1.6;}
+          x=xw;y=yw; ctx.fillStyle="green"; ctx.fillRect(x*50+260,-y*50+540,1,1);
+        }//fend i
+      }
+    }
+    render() {
+      return(
+        <div>
+          <canvas ref="canvas" width={640} height={425} />
+          <img ref="image" src="favicon.ico" alt="" style={{display: 'none'}} className="hidden" />
+        </div>
+      )
+    }
+  }
+export default BarnsleyFern;
