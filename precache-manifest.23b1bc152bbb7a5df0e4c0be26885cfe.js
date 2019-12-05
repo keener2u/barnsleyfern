e7755614b37fdf4990f8b56119da27ca71118bae
keener2u <keener2u@penguin>
2019-12-05 05:40:21 -0500

    Updates

---
+++ precache-manifest.23b1bc152bbb7a5df0e4c0be26885cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d227b971614d36d4bbb448e5cd5ea27",
+    "url": "/barnsleyfern/index.html"
+  },
+  {
+    "revision": "1d74e46347760d05fb41",
+    "url": "/barnsleyfern/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6c25c84c09a49db0e5c8",
+    "url": "/barnsleyfern/static/js/2.a6e306a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/barnsleyfern/static/js/2.a6e306a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d74e46347760d05fb41",
+    "url": "/barnsleyfern/static/js/main.66b2b619.chunk.js"
+  },
+  {
+    "revision": "f4173ec190595d13233f",
+    "url": "/barnsleyfern/static/js/runtime-main.32ff8527.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/barnsleyfern/static/media/logo.5d5d9eef.svg"
+  }
+]);
