5ac03cca7e35959954a9db0db3a288c5f42b6b3b
Matthew Keen <keener.2u@gmail.com>
2020-09-21 14:41:28 -0700

    Contact update (#7)
    * Branch to add contact info to site
    * changing site title from default
    * Added Shards UI Kit
    * Added container and initial navigation
    * Initial links and contact information updated in navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
-import React from 'react';
-import './App.css';
-import BarnsleyFern from './BarnsleyFern';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import "./App.css";
+import { Nav, NavItem, NavLink, Navbar } from "shards-react";
+import { Container, Row, Col } from "shards-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+import BarnsleyFern from "./BarnsleyFern";
+library.add(fab, fas);
 
 function App() {
   return (
-  <div className="App">
-  <BarnsleyFern width="640" height="425"/>
-  </div>
+    <div className="App">
+      <Container className="dr-example-container">
+        <Row>
+          <Col>
+            <BarnsleyFern width="640" height="425" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Navbar type="dark" theme="secondary" expand="md">
+              <Nav navbar className="mr">
+                <NavItem>
+                  <NavLink disabled href="#">
+                    Conservation through IOT
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="#">
+                    Remote Team Comradery
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="#">
+                    Past Projects
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <Nav navbar className="ml-auto">
+                <NavItem>
+                  <NavLink disabled href="#">
+                    <FontAwesomeIcon icon={["fab", "youtube-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="#">
+                    <FontAwesomeIcon icon={["fab", "twitter-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="mailto">
+                    <FontAwesomeIcon icon={["fab", "instagram-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://www.linkedin.com/in/matthew-keen-6ba886/">
+                    <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/keener2u">
+                    <FontAwesomeIcon icon={["fab", "github-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavLink
+                  active
+                  href="mailto:info@fractalfx.net?subject=FractalFX.net referral&body=Saw your website!"
+                >
+                  <FontAwesomeIcon icon={["fas", "envelope-square"]} />
+                </NavLink>
+              </Nav>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
