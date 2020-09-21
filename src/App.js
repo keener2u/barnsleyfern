b3250397e330dd0bb91f10d74d231530dedcfafe
keener2u <keener2u@penguin>
2020-09-21 14:26:25 -0700

    Initial links and contact information updated in navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,84 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "shards-ui/dist/css/shards.min.css"
-import './App.css';
-import { Nav, NavItem, NavLink } from "shards-react";
-import { Container, Row, Col} from "shards-react";
-import BarnsleyFern from './BarnsleyFern';
+import "shards-ui/dist/css/shards.min.css";
+import "./App.css";
+import { Nav, NavItem, NavLink, Navbar } from "shards-react";
+import { Container, Row, Col } from "shards-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
+import BarnsleyFern from "./BarnsleyFern";
+library.add(fab, fas);
 
 function App() {
   return (
-  <div className="App">
-    <Container className="dr-example-container">
+    <div className="App">
+      <Container className="dr-example-container">
         <Row>
-          <Col sm="12" lg="12"> <BarnsleyFern width="640" height="425"/></Col>
-          </Row>
-          <Row>
-            <Col sm={{ size: 8, order: 2, offset: 2}}>
-              <Nav>
-                  <NavItem>
-                    <NavLink active href="#">
-                      Active
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">Link</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="#">Another Link</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink disabled href="#">
-                      Disabled Link
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-              </Col>
-            </Row>
+          <Col>
+            <BarnsleyFern width="640" height="425" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Navbar type="dark" theme="secondary" expand="md">
+              <Nav navbar className="mr">
+                <NavItem>
+                  <NavLink disabled href="#">
+                    Conservation through IOT
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="#">
+                    Remote Team Comradery
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="#">
+                    Past Projects
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <Nav navbar className="ml-auto">
+                <NavItem>
+                  <NavLink disabled href="#">
+                    <FontAwesomeIcon icon={["fab", "youtube-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="#">
+                    <FontAwesomeIcon icon={["fab", "twitter-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink disabled href="mailto">
+                    <FontAwesomeIcon icon={["fab", "instagram-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://www.linkedin.com/in/matthew-keen-6ba886/">
+                    <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/keener2u">
+                    <FontAwesomeIcon icon={["fab", "github-square"]} />
+                  </NavLink>
+                </NavItem>
+                <NavLink
+                  active
+                  href="mailto:info@fractalfx.net?subject=FractalFX.net referral&body=Saw your website!"
+                >
+                  <FontAwesomeIcon icon={["fas", "envelope-square"]} />
+                </NavLink>
+              </Nav>
+            </Navbar>
+          </Col>
+        </Row>
       </Container>
-  </div>
+    </div>
   );
 }
 
--- src/BarnsleyFern.js
+++ src/BarnsleyFern.js
@@ -1,42 +1,67 @@
-import React from 'react';
+import React from "react";
 
 class BarnsleyFern extends React.Component {
-    componentDidMount() {
-      document.body.style.backgroundColor = "black"
-      /** Snippet from https://rosettacode.org/wiki/Barnsley_fern#JavaScript **/
-      const canvas = this.refs.canvas
-      const ctx = canvas.getContext("2d")
-      const img = this.refs.image
-      const lim = 10000;
-      img.onload = () => {
-        var w = canvas.width;
-        var h = canvas.height;
-        var x=0.,y=0.,xw=0.,yw=0.,r;
-        // Like in PARI/GP: return random number 0..max-1
-        function randgp(max) {return Math.floor(Math.random()*max)}
-        // Clean canvas
-        ctx.fillStyle="black"; ctx.fillRect(0,0,w,h);
-        // MAIN LOOP
-        ctx.font = "44px Courier"
-       
-        for(var i=0; i<lim; i++) {
-          r=randgp(100);
-          if (r<=1) {xw=0;yw=0.16*y;}
-          else if (r<=8) {xw=0.2*x-0.26*y;yw=0.23*x+0.22*y+1.6;}
-          else if (r<=15) {xw=-0.15*x+0.28*y;yw=0.26*x+0.24*y+0.44;}
-          else {xw=0.85*x+0.04*y;yw=-0.04*x+0.85*y+1.6;}
-          x=xw;y=yw; ctx.fillStyle="green"; ctx.fillRect(x*50+260,-y*50+540,1,1);
-        }//fend i
-        ctx.fillText("Fractal Fx", 10, 100)
-      } /** End snippet **/
-    }
-    render() {
-      return(
-        <div className="barnsleyfern">
-          <canvas ref="canvas" width={640} height={425} />
-          <img ref="image" src="favicon.ico" alt="" style={{display: 'none'}} className="hidden" />
-        </div>
-      )
-    }
+  componentDidMount() {
+    document.body.style.backgroundColor = "black";
+    /** Snippet from https://rosettacode.org/wiki/Barnsley_fern#JavaScript **/
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    const img = this.refs.image;
+    const lim = 10000;
+    img.onload = () => {
+      var w = canvas.width;
+      var h = canvas.height;
+      var x = 0,
+        y = 0,
+        xw = 0,
+        yw = 0,
+        r;
+      // Like in PARI/GP: return random number 0..max-1
+      function randgp(max) {
+        return Math.floor(Math.random() * max);
+      }
+      // Clean canvas
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, w, h);
+      // MAIN LOOP
+      ctx.font = "44px Courier";
+
+      for (var i = 0; i < lim; i++) {
+        r = randgp(100);
+        if (r <= 1) {
+          xw = 0;
+          yw = 0.16 * y;
+        } else if (r <= 8) {
+          xw = 0.2 * x - 0.26 * y;
+          yw = 0.23 * x + 0.22 * y + 1.6;
+        } else if (r <= 15) {
+          xw = -0.15 * x + 0.28 * y;
+          yw = 0.26 * x + 0.24 * y + 0.44;
+        } else {
+          xw = 0.85 * x + 0.04 * y;
+          yw = -0.04 * x + 0.85 * y + 1.6;
+        }
+        x = xw;
+        y = yw;
+        ctx.fillStyle = "green";
+        ctx.fillRect(x * 50 + 260, -y * 50 + 540, 1, 1);
+      } //fend i
+      ctx.fillText("Fractal Fx", 10, 100);
+    }; /** End snippet **/
   }
+  render() {
+    return (
+      <div className="barnsleyfern">
+        <canvas ref="canvas" width={640} height={425} />
+        <img
+          ref="image"
+          src="favicon.ico"
+          alt=""
+          style={{ display: "none" }}
+          className="hidden"
+        />
+      </div>
+    );
+  }
+}
 export default BarnsleyFern;
