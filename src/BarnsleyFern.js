e38a227338102a70d3e5450cb8bedb19704627cf
Matthew Keen <keener.2u@gmail.com>
2020-09-20 20:57:00 -0700

    Update BarnsleyFern.js
    Adding comment to external code.

--- src/BarnsleyFern.js
+++ src/BarnsleyFern.js
@@ -3,6 +3,7 @@ import React from 'react';
 class BarnsleyFern extends React.Component {
     componentDidMount() {
       document.body.style.backgroundColor = "black"
+      /** Snippet from https://rosettacode.org/wiki/Barnsley_fern#JavaScript **/
       const canvas = this.refs.canvas
       const ctx = canvas.getContext("2d")
       const img = this.refs.image
@@ -27,7 +28,7 @@ class BarnsleyFern extends React.Component {
           x=xw;y=yw; ctx.fillStyle="green"; ctx.fillRect(x*50+260,-y*50+540,1,1);
         }//fend i
         ctx.fillText("Fractal Fx", 10, 100)
-      }
+      } /** End snippet **/
     }
     render() {
       return(
