76f81452268284e19511331c629edb73c30f0bc4
Matthew Keen <keener.2u@gmail.com>
2020-09-26 22:10:39 -0700

    added a slider to change the limit of the code (#13)
    Co-authored-by: keener2u <keener2u@penguin>

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
 import "./App.css";
-import { Container, Row, Col } from "shards-react";
+import { Container, Row, Col, Slider } from "shards-react";
 
 
 import BarnsleyFern from "./BarnsleyFern";
@@ -10,13 +10,19 @@ import NavigationBar from "./NavigationBar"
 import MainContent from "./MainContent"
 function App() {
   const [collapsed, setCollapsed] = useState(false);
+  const [limit, setLimit] = useState(10000)
 
   return (
     <div className="App">
       <Container>
         <Row>
           <Col>
-            <BarnsleyFern width="640" height="425" />
+            <BarnsleyFern width="640" height="425" limit={limit} />
+            <Slider 
+              onSlide={(e) => setLimit(parseFloat(e[0]))} range={{min:200, max:40000}}
+              start={[limit]}
+              connect={[true, false]}
+            />
           </Col>
         </Row>
         <Row>
