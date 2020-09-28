5d168944aa467cf5f6a96be281503b64360e798c
keener2u <keener2u@penguin>
2020-09-28 10:38:15 -0700

    cleaning up

--- src/components/Link/Link.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Link = ({className, href, children}) => {
-  return <a data-testid="Link" className={className} href={href}>{children}</a>
-};
-
-
-export default Link;
