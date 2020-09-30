7a9ce6f6715600665f094d8401914875392e84f6
keener2u <keener2u@penguin>
2020-09-29 23:00:40 -0500

    moved around next projects page.

--- src/components/Comradery/Comradery.js
+++ src/components/Comradery/Comradery.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, CardTitle, CardBody, Row, Col, Container } from "shards-react";
 
 const Comradery = () => (
-  <div data-testid="Comradery" style={{color: 'black'}}>
+  <div data-testid="Comradery" style={{ color: "black" }}>
     <Card theme="light">
       <CardBody>
         <CardTitle>Value Proposition</CardTitle>
@@ -21,14 +21,14 @@ const Comradery = () => (
             <CardBody>
               <CardTitle>Teams Plugin: Live Agenda and The Conch</CardTitle>
               <p>
-                We have a layer 8 collision issue when using online tools. Based
-                off Lord of the Flies, this is a Microsoft Teams plugin that
-                enables people to click a button and enter a queue, when they
-                want to make a comment in the conversation. Once queued the
-                current speaker will have a small amount of time to finish up
-                their point before the conch moves to the next person. This
-                should enable fairness with online meetings and make sure the
-                person comfortable with breaking into a conversation doesn't
+                We have a layer 8 collision issue when using video conferencing
+                tools. Based off Lord of the Flies, this is a Microsoft Teams
+                plugin that enables people to click a button and enter a queue,
+                when they want to make a comment in the conversation. Once
+                queued the current speaker will have a small amount of time to
+                finish up their point before the conch moves to the next person.
+                This should enable fairness with online meetings and make sure
+                the person comfortable with breaking into a conversation doesn't
                 lead the chat.
               </p>
             </CardBody>
