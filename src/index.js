4ef0006fdebebcdca360354fc446f244007cf7ab
Matthew Keen <keener.2u@gmail.com>
2020-12-02 13:53:12 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-178650588-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
